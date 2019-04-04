9b55f52ec59e93c5b27dbf53d2be07ac02a9488b
richietam <richardtam.ny@gmail.com>
2019-04-03 23:32:08 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+var dodger = document.getElementById('dodger');
+
+
+
+  function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+
+  function moveDodgerRight() {
+
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+
+
+  }
+
+  document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+
+
+
+})
